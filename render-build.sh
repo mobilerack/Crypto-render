03992b74e89a733d6fc14e9668246e89f0604e15
mobilerack <93547594+mobilerack@users.noreply.github.com>
2025-09-05 00:52:42 +0200

    Delete render-build.sh

--- render-build.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env bash
-# exit on error
-set -o errexit
-
-# Függőségek telepítése
-pip install -r requirements.txt
-
-# Az adatbázis létrehozása és feltöltése
-python setup_database.py
